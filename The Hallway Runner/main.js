2783200b0fca72511daf37c7517cda682fc6383e
Stella Douka <35050451+stelladk@users.noreply.github.com>
2020-12-23 19:15:15 +0200

    Environment 3 added

--- The Hallway Runner/main.js
+++ The Hallway Runner/main.js
@@ -1,26 +1,32 @@
 let run;
-let rain;
-let ltng;
+let rain, ltng;
 let birds;
-let crickets;
-let owl;
+let crickets, owl;
+let dungeon;
 var loss = true;
 let sec = 0;
 
-var level = 0; //Change this to 0, 1 or 2 
+var level = 0; //Change this to 0, 1, 2 or 3
 
-let trees_l;
-let trees_r;
-var sky;
+let trees_l,trees_r;
+let panel_l, panel_r;
+let wall, wall_r;
 let fire, thorn, wood;
+
+let pirate;
 
 function preload() {
     trees_l = loadImage('assets/trees_l.jpg');
     trees_r = loadImage('assets/trees_r.jpg');
-    sky = loadImage('assets/sky.jpg');
+    panel_l = loadImage('assets/panel_l.jpg');
+    panel_r = loadImage('assets/panel_r.jpg');
+    wall = loadImage('assets/wall.png');
+    wall_r = loadImage('assets/wall_r.png');
     fire = loadImage('assets/fire.jpg');
     thorn = loadImage('assets/thorn.jpg');
     wood = loadImage('assets/wood.jpg');
+
+    pirate = loadModel('assets/pirate_body.obj', true);
 }
 
 function setup() {
@@ -32,6 +38,7 @@ function setup() {
     birds = createAudio('assets/birds.mp3');
     crickets = createAudio('assets/crickets.mp3');
     owl = createAudio('assets/owl.mp3');
+    dungeon = createAudio('assets/dungeon.wav');
     run.pause();
     run.volume(0.5);
     run.speed(1.2);
@@ -40,17 +47,27 @@ function setup() {
         crickets.pause();
         owl.pause();
         ltng.pause();
+        dungeon.pause();
     } else if (level == 1) {
         crickets.loop();
         owl.loop();
         birds.pause();
         ltng.pause();
+        dungeon.pause();
     } else if (level == 2) {
         rain.loop();
         rain.volume(0.4);
         birds.pause();
         crickets.pause()
         owl.pause();
+        dungeon.pause();
+    }else if (level == 3){
+        dungeon.loop();
+        dungeon.volume(0.4);
+        birds.pause();
+        crickets.pause();
+        owl.pause();
+        ltng.pause();
     }
 }
 
@@ -87,11 +104,14 @@ function draw() {
         light();
         hallway();
         sounds();
+    }else if(level == 3){
+        light3();
+        env3();
     }
     // hallway();
     // sounds();
     noStroke();
-    scale(1 / 4);
+    // scale(1 / 4);
     sec = millis() / 1000;
     modelGuy(pirate);
     // guyShape();
