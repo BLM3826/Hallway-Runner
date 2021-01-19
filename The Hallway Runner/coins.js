f220be294dc4e02782e7d52a2787cf26285578b6
Eleni Kougioumtzi <elkoum15@gmail.com>
2021-01-19 15:25:34 +0200

    add comments

--- The Hallway Runner/coins.js
+++ The Hallway Runner/coins.js
@@ -1,83 +1,65 @@
 var cpos; // coin position
 var zc = -270; // z of coin
 var speedc; // speed of coin
-var depth = 270;
-var shape = 2; // Shape of obstacle
-var filling = 0; //Fill obstacle
 var addcoin = true;
 
 function addcoins() {
     push();
-    if (int(cpos) == 0) {
+    
+    // Put coin in the right place (according to the value of cpos)   
+    // Set when the player takes the coin (different for each position)
+  
+    if (int(cpos) == 0) { // Up right
         translate(-25, -20, zc);
         if (zc >= 180 && zc <= 250 && side > 60 && jump <= -30) {
             if (addcoin) {
-                if(!mute) collect.play();
-                coins += 1;
-                //totalcoins += 1;
-                addcoin = false;
+                if(!mute) collect.play(); // Mute sound (when the player has muted the game from the menu)
+                coins += 1; // Increase coins by one
+                addcoin = false; // Do not increase again for this coin
             }
         }
-    } else if (int(cpos) == 1) {
+    } else if (int(cpos) == 1) { // Bottom right
         translate(-25, 30, zc);
         if (zc >= 180 && zc <= 250 && side > 60 && jump >= -10) {
             if (addcoin) {
                 if(!mute) collect.play();
                 coins += 1;
-                //totalcoins += 1;
                 addcoin = false;
             }
         }
-    } else if (int(cpos) == 2) {
+    } else if (int(cpos) == 2) { // Bottom left
         translate(-90, 30, zc);
         if (zc >= 180 && zc <= 250 && side < 40 && jump >= -10) {
             if (addcoin) {
                 if(!mute) collect.play();
                 coins += 1;
-                //totalcoins += 1;
                 addcoin = false;
             }
         }
-    } else {
+    } else { // Up left
         translate(-90, -20, zc);
         if (zc >= 180 && zc <= 250 && side < 40 && jump <= -30) {
             if (addcoin) {
                 if(!mute) collect.play();
                 coins += 1;
-                //totalcoins += 1;
                 addcoin = false;
             }
         }
     }
 
-    if (zc > 270) {
-        //cpos = random(4); //Starting obstacles from different positions
-        cpos = random(4);
-        zc = -270;
-        addcoin = true;
+    if (zc > 270) { // Coin has disappeared from the screen
+        cpos = random(4);// Starting coins from 4 different positions
+        zc = -270; // Start next coin at maximum depth
+        addcoin = true; // Next coin will be able to increase coins
     }
-    if (loss === false) {
+    if (loss === false) { // Move coin 
         zc += speedc;
     }
-    /*if (loss === false && speed <= 10 && no == 5){
-      nc = 0; //reset
-      speedc += 1; // increase speed every 5 obstacles
-    }*/
 
-    /*normalMaterial();
-    //texture(coin);
-    if(addcoin){
-      fill(255, 255, 0);
-    }else{
-      noFill();
-    }
-    //sphere(6);
-    scale(1 / 5);
-    model(coin);*/
+    // Create coin
     if (addcoin) {
         normalMaterial();
         fill(255, 200, 0);
-        //fill(255);
         scale(1 / 12);
         model(coin);
     }
