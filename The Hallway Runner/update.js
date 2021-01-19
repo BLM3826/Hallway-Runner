c702a69df98a5cea3fc416747b55447154939a7f
Eleni Kougioumtzi <elkoum15@gmail.com>
2021-01-19 15:27:31 +0200

    add comments

--- The Hallway Runner/update.js
+++ The Hallway Runner/update.js
@@ -2,10 +2,10 @@
 function score_coins() {
     totalno = score;
     if (totalno > 0) {
-        if (score > hscore) {
+        if (score > hscore) { // New highscore message
             span_highscore.classList.add("newrecord");
-            score_msg.innerHTML = "New Highscore!";
-        } else if (totalno % 30 == 0) {
+            score_msg.innerHTML = "New Highscore!";          
+        } else if (totalno % 30 == 0) { // Encouraging messages
             score_msg.innerHTML = "You are unstoppable!";
         } else if (totalno % 20 == 0) {
             score_msg.innerHTML = "Keep Going!";
@@ -13,19 +13,20 @@ function score_coins() {
             score_msg.innerHTML = "Good Job!";
         }
     } else {
-        score_msg.innerHTML = "Have Fun!";
+        score_msg.innerHTML = "Have Fun!"; // Initial message
     }
-    span_score.innerHTML = score;
-    span_coins.innerHTML = coins;
-    span_highscore.innerHTML = hscore;
-    span_total_coins.innerHTML = totalcoins;
+    span_score.innerHTML = score; // Set score
+    span_coins.innerHTML = coins; // Set coins
+    span_highscore.innerHTML = hscore; // Set highscore
+    span_total_coins.innerHTML = totalcoins; // Set total coins
 
 }
 
 // Update position of the walls
 function moveWalls() {
     if (loss === false) {
-        translate_z += 3 * (1 + (speed - 4) / 10);
+        // Wall speed depends on the speed of the enemies
+        translate_z += 3 * (1 + (speed - 4) / 10); 
 
         if (translate_z > 500) {
             translate_z = 0;
